39a760fc018962c3b2e173774f0a50ce61253cc5
xujunmeng.github <xujunmeng2012@163.com>
2020-03-25 11:06:51 +0800

    update

--- src/main/java/junmeng/config/RmiConfig.java
+++ src/main/java/junmeng/config/RmiConfig.java
@@ -4,8 +4,7 @@ import junmeng.common.IUserService;
 import junmeng.common.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.remoting.rmi.RmiServiceExporter;
-import org.springframework.remoting.support.RemoteExporter;
+import org.springframework.remoting.caucho.HessianServiceExporter;
 
 /**
  * @author james
@@ -14,20 +13,32 @@ import org.springframework.remoting.support.RemoteExporter;
 @Configuration
 public class RmiConfig {
 
+//    /**
+//     * 将userService暴露为远程服务
+//     * @return
+//     */
+//    @Bean
+//    RemoteExporter registerRMIExporter() {
+//        RmiServiceExporter exporter = new RmiServiceExporter();
+//        //这里的服务名必须和客户端调用协议rmi://hostname:1199/*** 的***相同
+//        exporter.setServiceName("userRmiService");
+//        exporter.setServiceInterface(IUserService.class);
+//        exporter.setService(new UserService());
+//        //端口号，默认为1099
+//        exporter.setRegistryPort(1199);
+//        return exporter;
+//    }
+
     /**
      * 将userService暴露为远程服务
-     * @return
      */
-    @Bean
-    RemoteExporter registerRMIExporter() {
-        RmiServiceExporter exporter = new RmiServiceExporter();
-        //这里的服务名必须和客户端调用协议rmi://hostname:1199/*** 的***相同
-        exporter.setServiceName("userRmiService");
-        exporter.setServiceInterface(IUserService.class);
-        exporter.setService(new UserService());
-        //端口号，默认为1099
-        exporter.setRegistryPort(1199);
-        return exporter;
+    @Bean(name = "userHessianService")
+    HessianServiceExporter registerHessianExporter() {
+        HessianServiceExporter hessianServiceExporter = new HessianServiceExporter();
+        hessianServiceExporter.setService(new UserService());
+        hessianServiceExporter.setServiceInterface(IUserService.class);
+        return hessianServiceExporter;
     }
+
 
 }
